626906ce3eae8c2655934c715405400e3f79c657
Catalin <lungancatalin@yahoo.com>
2020-04-28 20:51:51 +0300

    Updates

---
+++ precache-manifest.f0193f77ee2df5661926bd1290c77e79.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdbe324636be9867e470be2838b2e77",
+    "url": "/base_demo/index.html"
+  },
+  {
+    "revision": "7d1cd7a81c6de1b83fa2",
+    "url": "/base_demo/static/css/main.b7de0216.chunk.css"
+  },
+  {
+    "revision": "d3e8e0bd984a6db97ecd",
+    "url": "/base_demo/static/js/2.ab7f327a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/base_demo/static/js/2.ab7f327a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d1cd7a81c6de1b83fa2",
+    "url": "/base_demo/static/js/main.80ba4b06.chunk.js"
+  },
+  {
+    "revision": "be3eeaaa3dfd1928b02e",
+    "url": "/base_demo/static/js/runtime-main.e799bd4d.js"
+  },
+  {
+    "revision": "1d55799afb7254685302a38efea12fd1",
+    "url": "/base_demo/static/media/hero-mobile.1d55799a.jpg"
+  },
+  {
+    "revision": "69950c2de382a8d4924260343e516fad",
+    "url": "/base_demo/static/media/icon-arrow.69950c2d.svg"
+  },
+  {
+    "revision": "337809979308399afb36fa5f0e3fbcdf",
+    "url": "/base_demo/static/media/icon-error.33780997.svg"
+  },
+  {
+    "revision": "877543ce69f4ed6f834f26e7dc4ca930",
+    "url": "/base_demo/static/media/logo.877543ce.svg"
+  }
+]);
